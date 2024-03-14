374e7274a65e123313a30d33ebaf604dae7fa1e1
pavan <pavanskull@gmail.com>
2024-03-14 16:29:55 +0000

    square script added

---
+++ square.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# Input number
+my $number = 5;
+
+# Calculate square
+my $square = $number * $number;
+
+# Print result
+print "Square of $number is: $square\n";
+
